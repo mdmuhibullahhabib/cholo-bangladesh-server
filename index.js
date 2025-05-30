dfe4947d2622d5ff18d841e9846c96cda062ad7a
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-05-31 00:31:03 +0600

    Final

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const { ServerApiVersion } = require('mongodb')
 app.use(cors());
 app.use(express.json())
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5eri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
